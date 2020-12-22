ead085250b3a0333da337110554d3f5febf27632
Gar <gar@lonelydatu.com>
2020-12-22 13:08:21 -0500

    on

--- dev/raptors_300x250/_bundled/log-free.js
+++ dev/raptors_300x250/_bundled/log-free.js
@@ -19,13 +19,14 @@ var _commonJsCommonJs = require('../../_common/js/common.js');
 function start() {
 	TweenLite.defaultEase = Power4.easeOut;
 	var tl = new TimelineMax();
-	var speed = .3;
+	var speed = .4;
 	tl.set(".frame1", { opacity: 1 });
 	tl.to(".t1", .3, { y: 90 }, "+=.3");
-	tl.add("one", "+=1");
-	tl.to(".t1", speed, { y: 0 }, "one");
-	tl.to(".w", speed, { y: 0 }, "one");
-	tl.to(".person", speed, { y: 0 }, "one+=.5");
+	tl.add("one", "+=.7");
+	tl.to(".t1", speed * .5, { y: 0 }, "one");
+	tl.to(".w", speed, { y: 0, ease: Back.easeOut }, "one");
+
+	tl.to(".person", speed, { y: 0, ease: Power4.easeIn }, "one+=.6");
 }
 
 start();
--- dev/raptors_300x250/_js/main.js
+++ dev/raptors_300x250/_js/main.js
@@ -3,13 +3,14 @@ import {size} from '../../_common/js/common.js'
 function start(){
 	TweenLite.defaultEase = Power4.easeOut
 	const tl = new TimelineMax()
-	const speed = .3
+	const speed = .4
 	tl.set(".frame1", {opacity:1})
 	tl.to(".t1", .3, {y:90}, "+=.3")
-	tl.add("one", "+=1")
-	tl.to(".t1", speed, {y:0}, "one")
-	tl.to(".w", speed, {y:0}, "one")
-	tl.to(".person", speed, {y:0}, "one+=.5")
+	tl.add("one", "+=.7")
+	tl.to(".t1", speed*.5, {y:0}, "one")
+	tl.to(".w", speed, {y:0, ease:Back.easeOut}, "one")
+
+	tl.to(".person", speed, {y:0, ease:Power4.easeIn}, "one+=.6")
 	
 	
 	
