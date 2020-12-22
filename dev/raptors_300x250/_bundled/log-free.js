8ff051c6c751475cb3716d5ba63a327525cd46c7
Gar <gar@lonelydatu.com>
2020-12-22 13:17:15 -0500

    on

--- dev/raptors_300x250/_bundled/log-free.js
+++ dev/raptors_300x250/_bundled/log-free.js
@@ -22,7 +22,7 @@ function start() {
 	var speed = .4;
 	tl.set(".frame1", { opacity: 1 });
 	tl.to(".t1", .3, { y: 90 }, "+=.3");
-	tl.add("one", "+=.7");
+	tl.add("one", "+=.6");
 	tl.to(".t1", speed * .5, { y: 0 }, "one");
 	tl.to(".w", speed, { y: 0, ease: Back.easeOut }, "one");
 
